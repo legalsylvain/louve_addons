44f77ee4885c8b772eefe35cb451b89c7ac0508d
jweste <julien.weste@akretion.com.br>
2016-02-29 21:41:43 +0100

    [FIX] Remove useless view

--- product_average_consumption/__openerp__.py
+++ product_average_consumption/__openerp__.py
@@ -46,7 +46,6 @@ Copyright, Author and Licence :
         'stock',
     ],
     'data': [
-        'views/product_product_view.xml',
         'views/product_template_view.xml',
         'views/res_config_view.xml',
     ],
