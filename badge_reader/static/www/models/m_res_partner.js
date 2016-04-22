13059b6358da529d092a71893d4ca8007bc8bdf5
Sylvain LE GAL <sylvain.legal@grap.coop>
2016-04-22 11:56:59 +0200

    [FIX] various;

--- badge_reader/static/www/models/m_res_partner.js
+++ badge_reader/static/www/models/m_res_partner.js
@@ -9,7 +9,7 @@ angular.module('starter').factory('ResPartnerModel', ['$q', 'jsonRpc', function 
                 'id', 'name', 'street', 'street2', 'zip', 'city', 'customer', 'country_id', 'phone', 'mobile', 'bootstrap_state', 'state']).then(function (partner_res) {
                 if (partner_res.records.length == 1){
                     var res = partner_res.records[0];
-                    res.image_url = "web/image?model=res.partner&id=" + partner_id + "&field=image";
+                    res.image_url = "../../../web/image?model=res.partner&id=" + partner_id + "&field=image";
                     return res;
                 }else{
                     return false;
