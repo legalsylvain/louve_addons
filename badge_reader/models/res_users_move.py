7f87412b866bd4d26962598bcb654f003fffa064
jweste <julien.weste@akretion.com.br>
2016-08-18 10:08:01 +0200

    [PEP8]

--- badge_reader/models/res_users_move.py
+++ badge_reader/models/res_users_move.py
@@ -3,12 +3,13 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import fields, models, api
+from openerp import fields, models
 
 from openerp.addons.badge_reader.models.res_partner import (
     _BADGE_PARTNER_BOOTSTRAP_STATE,
     _BADGE_PARTNER_STATE,
 )
+
 
 class ResUsersMove(models.Model):
     _name = 'res.users.move'
