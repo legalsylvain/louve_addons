bb75414dbdccfa5fa661554f31a88648e9f54d9f
jweste <julien.weste@akretion.com.br>
2016-09-01 19:05:42 +0200

    [FIX] force french date format

--- coop_shift/report/report_wallchart_template.py
+++ coop_shift/report/report_wallchart_template.py
@@ -144,9 +144,10 @@ class ReportWallchartTemplate(models.AbstractModel):
         for i in range(4):
             delta = (i + 1 - week_number[0]) % 4
             result["week" + ["A", "B", "C", "D"][i]] = "(%s, %s, ...)" % (
-                datetime.strftime(next_date + timedelta(weeks=delta), "%x"),
                 datetime.strftime(
-                    next_date + timedelta(weeks=delta + 4), "%x"),)
+                    next_date + timedelta(weeks=delta), "%d/%m/%Y"),
+                datetime.strftime(
+                    next_date + timedelta(weeks=delta + 4), "%d/%m/%Y"),)
         return result
 
     @api.multi
