712fe5b25e6584734d61b5767c94522eb766cc00
Sylvain LE GAL <sylvain.legal@grap.coop>
2016-09-04 15:49:23 +0200

    [ADD] new module capital souscription

---
+++ capital_souscription/models/account_journal.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2016-Today: La Louve (<http://www.lalouve.net/>)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, fields
+
+
+class AccountJournal(models.Model):
+    _inherit = 'account.journal'
+
+    # Column Section
+    is_capital_souscription = fields.Boolean(
+        string='Used for Capital Souscription')
