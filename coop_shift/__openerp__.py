fc647066f8c2dc33fe7b1cc288ac48c587947e92
jweste <julien.weste@akretion.com.br>
2016-09-18 14:03:59 +0200

    [FIX] show all shift.registration for all shift users

--- coop_shift/__openerp__.py
+++ coop_shift/__openerp__.py
@@ -74,7 +74,6 @@ Copyright, Author and Licence :
         'views/res_partner_view.xml',
         'security/ir_model_access_data.yml',
         'security/ir_rule_data.yml',
-        'security/ir_rule_data.xml',
         'data/module_data.xml',
         'views/shift_report.xml',
         'views/report_timesheet.xml',
