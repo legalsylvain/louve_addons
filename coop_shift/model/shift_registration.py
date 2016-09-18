04347526089805f116274efed35679af1c48006b
jweste <julien.weste@akretion.com.br>
2016-09-18 22:51:45 +0200

    [IMP] check the number of shift.registration in the same day and in the preceding X days (for legal reasons)

--- coop_shift/model/shift_registration.py
+++ coop_shift/model/shift_registration.py
@@ -23,6 +23,7 @@
 
 from openerp import models, fields, api, _
 from openerp.exceptions import UserError
+from datetime import timedelta
 
 STATES = [
     ('cancel', 'Cancelled'),
@@ -35,6 +36,10 @@ STATES = [
     ('replaced', 'Replaced'),
     ('replacing', 'Replacing'),
 ]
+
+MAX_REGISTRATIONS_PER_DAY = 2
+MAX_REGISTRATION_PER_PERIOD = 5
+NUMBER_OF_DAYS_IN_PERIOD = 28
 
 
 class ShiftRegistration(models.Model):
@@ -104,3 +109,31 @@ class ShiftRegistration(models.Model):
         for reg in self:
             reg.shift_ticket_id = reg.shift_id.shift_ticket_ids.filtered(
                 lambda t: t.product_id == FTOP_product)
+
+    @api.model
+    def create(self, vals):
+        partner_id = vals.get('partner_id', False)
+        partner = self.env['res.partner'].browse(partner_id)
+        date_reg = vals.get('date_begin', False)
+        if date_reg:
+            date_reg = fields.Date.from_string(date_reg)
+            regs = partner.registration_ids.filtered(
+                lambda r, d=date_reg:
+                fields.Date.from_string(r.date_begin) == d and
+                r.state != 'cancel')
+            if len(regs) >= MAX_REGISTRATIONS_PER_DAY:
+                raise UserError(_("""This member already has %s registrations\
+                in the same day. You can't program more.""") % len(regs))
+            check_begin_date = date_reg - timedelta(
+                days=NUMBER_OF_DAYS_IN_PERIOD - 1)
+            regs = partner.registration_ids.filtered(
+                lambda r, d1=check_begin_date, d2=date_reg:
+                fields.Date.from_string(r.date_begin) >= d1 and
+                fields.Date.from_string(r.date_begin) <= d2 and
+                r.state != 'cancel')
+            if len(regs) >= MAX_REGISTRATION_PER_PERIOD:
+                raise UserError(_(
+                    """This member already has %s registrations in the\
+                    preceding %s days. You can't program more.""") % (
+                    len(regs), NUMBER_OF_DAYS_IN_PERIOD))
+        return super(ShiftRegistration, self).create(vals)
